ceb2d5fa1019b84e038803f35e467dff8ce6ed49
elias vasquez roca <eliasvroca@gmail.com>
2025-08-13 15:53:52 -0400

    change: cambiando la url de registro

--- src/components/mobile/ListMenu.jsx
+++ src/components/mobile/ListMenu.jsx
@@ -45,7 +45,7 @@ export const ListMenu = ({ menuOff }) => {
                             <path d="M10 15v-6h2a2 2 0 1 1 0 4h-2"></path>
                             <path d="M14 15l-2 -2"></path>
                         </svg>
-                        <a href="https://portal.shirelediciones.com/sesion_register/singup.php"
+                        <a href="https://portal.shirelediciones.com/auth/signup"
                         >Regístrese
                         </a>
                     </li>
